b5ead7b2a39752cab5f2330fa5c55f7c3f729c53
Shane Rachal <imspammails31@gmail.com>
2021-03-07 01:43:42 -0600

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 4, "M", "Dark Brown", "Protective", TRUE);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Rowdy", 2, "M", "Brown", "Playful", TRUE);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Tabitha", 3, "F", "Black", "Fearful", TRUE);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 6, "M", "Brown", "Strict", TRUE);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Melissa", 4, "F", "Light Brown", "Friendly", TRUE);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Grinch", 2, "M", "Hazel", "Sensative", TRUE);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES ("Wendy", 5, "F", "Light Brown", "Friendly", TRUE);
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES (NULL, 3, "M", "Black", "Malicious", FALSE)
